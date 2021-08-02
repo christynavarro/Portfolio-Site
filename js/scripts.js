4fb132f0ae5124fbe1acf49e06e93c74675f813b
christynavarro <christymarieee@gmail.com>
2021-08-02 12:49:58 -0400

    Added in photos for projects

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
 console.log("Hello world")
+var age = 39;
+var name = "John";
+var personParent = {
+  name: name,
+  age: age,
+  child: {
+    name: "Sara"
+  }
+};
+personParent.name = "Anne"; // Set the name property to "Anne"
+personParent.child.size = 175; // Set new values that weren't set before
+console.log(personParent.child.name); // Output "Sara" to the console
+delete shortObject.name; //Will remove the name propery
