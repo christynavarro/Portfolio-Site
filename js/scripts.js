afb8029a241088355b948dea41fc23b888ff7892
christynavarro <christymarieee@gmail.com>
2021-08-04 18:03:16 -0400

    Added hamburger icon

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,8 @@
-console.log("Hello world")
-var age = 39;
-var name = "John";
-var personParent = {
-  name: name,
-  age: age,
-  child: {
-    name: "Sara"
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
   }
-};
-personParent.name = "Anne"; // Set the name property to "Anne"
-personParent.child.size = 175; // Set new values that weren't set before
-console.log(personParent.child.name); // Output "Sara" to the console
-delete shortObject.name; //Will remove the name propery
+}
